c9ea44fb22eeef21223402f0e3a9460151bfa9f1
feellight79 <jak93503@example.com>
2025-03-02 07:04:32 -0800

    users

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
 import LeftSide from './LeftSide';
 import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight } from 'lucide-react';
+import { SearchIcon } from 'lucide-react';
 
 const generateData = () => {
     return Array.from({ length: 100 }, (_, index) => ({
@@ -47,13 +48,35 @@ function Users() {
     const [activeview, setActiveView] = useState([]);
     const handleView = (index) => {
         const updatedActiveView = [...activeview];
-        console.log(updatedActiveView);
         updatedActiveView[index] = !updatedActiveView[index];
-        console.log(updatedActiveView);
         setActiveView(updatedActiveView);
     };
-    console.log(activeview);
 
+    const [activecheck, setActiveCheck] = useState([]);
+    const handleCheck = (index) => {
+        const updatedActiveCheck = [...activecheck];
+        updatedActiveCheck[index] = !updatedActiveCheck[index];
+        setActiveCheck(updatedActiveCheck);
+    };
+
+    // Search bar
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchview, setSearchView] = useState(false);
+
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value);
+    };    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Implement search functionality here
+        console.log('Search term submitted:', searchTerm);
+    };
+    const handleSearch=() => {
+        var updatesearchbar = searchview;
+        updatesearchbar = !updatesearchbar;
+        setSearchView(updatesearchbar);
+    }    
+    console.log(searchview);
 
     return (
         <div className='h-screen relative'>
@@ -65,8 +88,8 @@ function Users() {
                     <Link className='left-0 top-0 w-full' to={"/"}>
                         <p className='text-[20px] text-start leading-[60px] ml-[53px] mr-[45px] border-b-2 border-b-[#5C4435]'><span className='text-[30px] pr-[5px]'>&larr;</span> All Templates /</p>
                     </Link>
-                    <div className="p-[45px] flex-1 flex flex-col">
-                        <div className="flex justify-start items-center ">
+                    <div className="pl-[45px] pr-[10px] py-[45px] xl:p-[45px] flex-1 flex flex-col m-auto 2xl:w-[1220px] xl:w-[1000px] lg:w-[760px] md:w-[530px]">
+                        <div className="flex justify-start items-end ">
                             <div className="flex flex-col gap-4">
                                 <label className="text-sm text-[#5d5e5f]">Label</label>
                                 <div className="flex items-center justify-between px-4 py-3 bg-gradient-to-b from-[#242F4D] to-[#38466B] text-white rounded-[18px] w-[150px] h-[38px] cursor-pointer">
@@ -90,7 +113,23 @@ function Users() {
                                         <span className='right mr-2'>&#9013;</span>
                                     </div>
                                 </div>
-                            </div>                            
+                            </div>    
+                            <SearchIcon onClick={() => handleSearch()} className='text-white bg-[#6F8185] px-3.5 h-[38px] w-12 rounded-md'/>       
+                            <form className={`ml-4 flex items-center ${searchview? 'block w-[200px]':'hidden w-0'}`} onSubmit={handleSubmit}>
+                                <input
+                                    type="text"
+                                    value={searchTerm}
+                                    onChange={handleChange}
+                                    placeholder="Search..."
+                                    className="w-full p-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                />
+                                {/* <button
+                                    type="submit"
+                                    className="p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                >
+                                    Search
+                                </button> */}
+                            </form>                
                         </div>
                         <div className='flex-1 flex flex-col w-full'>
                             <div className="relative flex-1 flex flex-col border border-[#5C4435] max-w-6xl bg-white rounded-lg shadow-md mt-4">                                                                                    
@@ -111,9 +150,19 @@ function Users() {
                                                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{item.Username}</td>
                                                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{item.Role}</td>
                                                 <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{item.Company}</td>
-                                                <td>
+                                                <td className='flex justify-start items-center gap-2 py-[10px]'>
                                                     <button key={index} onClick={() => handleView(index)} class="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg]:size-4 [&amp;_svg]:shrink-0 hover:bg-accent hover:text-accent-foreground h-8 w-8">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class={`viewf lucide lucide-eye h-4 w-4 ${activeview[index]? 'text-green-500':'text-gray-800'}`}><path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path><circle cx="12" cy="12" r="3"></circle></svg>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class={`lucide lucide-eye h-5 w-5 ${activeview[index]? 'text-green-600':'text-gray-800'}`}><path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path><circle cx="12" cy="12" r="3"></circle></svg>
+                                                    </button>
+                                                    <button onClick={() => handleCheck(index)} class="h-8 w-8 flex items-center justify-center rounded-md hover:bg-[#f6f2ef]">
+                                                        <svg className={`text-red-600 h-5 w-5 ${activecheck[index]? 'hidden':'block'}`} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                            <circle cx="12" cy="12" r="10" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></circle>
+                                                            <line x1="4.93934" y1="4.93934" x2="19.0607" y2="19.0607" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></line>
+                                                        </svg>
+                                                        <svg className={`text-green-600 h-5 w-5 ${activecheck[index]? 'block':'hidden'}`} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                            <path d="M22 11.08V12C21.9988 14.1564 21.3005 16.2547 20.0093 17.9818C18.7182 19.709 16.9033 20.9725 14.8354 21.5839C12.7674 22.1953 10.5573 22.1219 8.53447 21.3746C6.51168 20.6273 4.78465 19.2461 3.61096 17.4371C2.43727 15.628 1.87979 13.4881 2.02168 11.3363C2.16356 9.18455 2.99721 7.13631 4.39828 5.49706C5.79935 3.85781 7.69279 2.71537 9.79619 2.24013C11.8996 1.7649 14.1003 1.98232 16.07 2.85999" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                                            <path d="M22 4L12 14.01L9 11.01" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                                        </svg>
                                                     </button>
                                                 </td>
                                             </tr>
@@ -233,45 +282,6 @@ function Users() {
                                 </div>
                             </div>
                         </div>
-                        {/* <div className='border border-[#5C4435] rounded-lg mt-8 max-w-[1300px] m-auto p-1'>
-                            <table className="w-full border-collapse text-base leading-[49px]">
-                                <thead className='text-[#5C4435] font-bold'>
-                                    <tr>
-                                        <th className="text-left font-medium px-[25px]">Username</th>
-                                        <th className="text-left font-medium">Role</th>
-                                        <th className="text-left font-medium">Company</th>
-                                        <th className="w-24"></th>
-                                    </tr>
-                                </thead>
-                                <tbody className='border-y border-[#A77C61]'>
-                                    <tr className='border-y border-[#A77C61] bg-[#F6F2EF]'>
-                                        <td className='px-[25px]'>Admin1</td>
-                                        <td>SuperAdmin</td>
-                                        <td>-</td>
-                                        <td>
-                                            <button class="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg]:size-4 [&amp;_svg]:shrink-0 hover:bg-accent hover:text-accent-foreground h-8 w-8"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye h-4 w-4"><path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path><circle cx="12" cy="12" r="3"></circle></svg></button>
-                                        </td>
-                                    </tr>
-                                    <tr className='border-y border-[#A77C61]'>
-                                        <td>Admin1</td>
-                                        <td>SuperAdmin</td>
-                                        <td>-</td>
-                                        <td>
-                                            <button class="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg]:size-4 [&amp;_svg]:shrink-0 hover:bg-accent hover:text-accent-foreground h-8 w-8"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye h-4 w-4"><path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path><circle cx="12" cy="12" r="3"></circle></svg></button>
-                                        </td>
-                                    </tr>
-                                    <tr className='border-y border-[#A77C61]'>
-                                        <td>Admin1</td>
-                                        <td>SuperAdmin</td>
-                                        <td>-</td>
-                                        <td>
-                                            <button class="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg]:size-4 [&amp;_svg]:shrink-0 hover:bg-accent hover:text-accent-foreground h-8 w-8"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye h-4 w-4"><path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path><circle cx="12" cy="12" r="3"></circle></svg></button>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                                 
-                            </table>
-                        </div> */}
                     </div>
                 </div>
             </div>
